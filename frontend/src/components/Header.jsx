3c2fb339cd78bbea216933978dc28b9f7c6c4c96
balachandar <balachandhar@gmail.com>
2025-01-30 22:52:12 +0530

    logout functionality added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,10 @@ import "../css/nav.css";
 import img from "../assets/img.jpg"
 
 const Header = () => {
+  const showlogout=()=>{
+    var logout=document.getElementById("logout");
+    logout.style.display = logout.style.display === "flex" ? "none" : "flex";
+  }
   return (
     <nav className="navbar">
       <div className="logo">
@@ -14,7 +18,10 @@ const Header = () => {
         <li><Link to="/templates">Create Resume</Link></li>
       </ul>
       <div className="profile">
-          <img src={img} alt="Profile" />
+          <img src={img} alt="Profile"  onClick={showlogout}/>
+      </div>
+      <div className="logout" id="logout">
+        <h3><Link className="logout_link" to='/' >logout</Link></h3>
       </div>
     </nav>
   );
