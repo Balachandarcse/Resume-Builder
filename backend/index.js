4178983e1fd65a541914f0dfbc6458489a794639
balachandar <balachandhar@gmail.com>
2025-01-30 13:14:29 +0530

    cors access contraol error fixed 2

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app=express()
 
 dotenv.config();
 app.use(cors({
-    origin:'https://resume-builder-azure-three.vercel.app/'
+    origin:'https://resume-builder-azure-three.vercel.app'
     
 }));
 app.use(express.urlencoded());
